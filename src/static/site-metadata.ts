5cbb2572cf9cb2d377fc575a77b59f0fc40b4ccb
bzzd2001 <98963764+bzzd2001@users.noreply.github.com>
2025-04-10 17:03:02 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,8 +10,8 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'ZiToneRunning Page',
-  siteUrl: 'https://yihong.run',
+  siteTitle: 'ZiTone Running',
+  siteUrl: 'https://bzzd2001.github.io/running_page/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
@@ -21,7 +21,7 @@ const data: ISiteMetadataResult = {
     },
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: 'https://bzzd2001.github.io/running_page/',
     },
     {
       name: 'About',
